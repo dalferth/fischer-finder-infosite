f56bbdd3c986a9ece3d92de140c1c7a144c0f065
bd <51322242+bdwebnet@users.noreply.github.com>
2023-04-18 16:24:20 +0200

    text update

--- pages/index.vue
+++ pages/index.vue
@@ -94,8 +94,8 @@
                         von TensorFlow zu <strong>konkreten Produktmerkmalen</strong> verarbeitet.
                     </li>
                     <li>
-                        Mithilfe der konkreten Produktmerkmalen wird dann versucht ein Produkt in der aktuellen bzw.
-                        später optimierten <strong>fischer-API</strong> (s. Optimierung bestehender Daten) / <strong>Decision
+                        Mithilfe der konkreten Produktmerkmalen wird dann versucht, ein Produkt in der aktuellen bzw.
+                        später optimierten <strong>fischer-API</strong> (s. <a href="#optimierungBestehenderDaten">Optimierung bestehender Daten</a>) / <strong>Decision
                                                                                                                      Tree</strong>.
                     </li>
                     <li>
